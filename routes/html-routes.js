0c8196c95a4841db2a1c07a43fae2000e009da3f
Life is Now <keylasan28@yahoo.com>
2020-04-08 19:49:01 -0400

    html routes with index and gigs

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,24 @@
-// Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
 
+// Each of the below routes just handles the HTML page that the user gets sent to.
+module.exports = function(app) {
+  // index route loads..
+  app.get("/", function(req, res) {
+    res.render("index");
+  });
+
+  app.get("/", function(req, res) {
+    res.render("gigs");
+  });
+};
+
+
+
+
+// Requiring path to so we can use relative routes to our HTML files
+//var path = require("path");
+
+/*
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
@@ -29,3 +47,6 @@ module.exports = function(app) {
   });
 
 };
+
+*/
+
