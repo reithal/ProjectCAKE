77486ad3f1c99a188890570b3ac3b67ee610b4a6
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-04-13 00:50:34 -0400

    updated apply page to create applicant, alert on success and update the gig with applicant id. Also created new handlebars for login signup

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
   // Getting references to our form and inputs
-  var loginForm = $("form.login");
+  var loginBtn = $("button#login");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
   // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function(event) {
+  loginBtn.on("click", function(event) {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
-
+   
     if (!userData.email || !userData.password) {
       return;
     }
 
     // If we have an email and password we run the loginUser function and clear the form
+   //console.log(userData);
     loginUser(userData.email, userData.password);
     emailInput.val("");
     passwordInput.val("");
